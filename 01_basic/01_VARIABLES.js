15dc6b25ecaa8e927297415a78d4364acbf9721e
ts3599920-a11y <ts3599920@gmail.com>
2026-04-04 10:32:12 +0000

    variable

---
+++ 01_basic/01_VARIABLES.js
@@ -0,0 +1,23 @@
+const accountId = 555
+//const ka use constant (fix) value store karne ke liye hota hai.//
+
+let accountemail = "shaikhrariqu@.com"
+//let ka use aisi value ke liye hota hai jo future mein change ho sakti hai//
+
+var accountname = "tarique"
+// ye old time main use hota tha ab use mat karna//
+
+accountcity = "delhi"
+//bina var,let ke bhi data bana sakte hai//
+
+//now lets change their value//
+
+ accountemail= "jareer@.com"
+ accountname = "jareer"
+accountcity = "gujrat"
+
+//console.log();//
+//ye print ke liye kaam aaata hai//
+
+console.table([accountemail,accountname,accountcity])
+//console table se ek saath sab print ho jata haivo bhi table structure main //
